7ba60d3d523d1d4c24d24912a50c79c74e6f173b
Leandro Abel Maro <leandro.maro@glb-d1135.globant.com>
2015-10-26 10:56:10 -0300

    removing unuseful lines

--- rest.pl
+++ rest.pl
@@ -2,25 +2,17 @@
 
   #Create a user agent object
   use LWP::UserAgent;
-  #my $user_agent = LWP::UserAgent::JSON->new;
   my $ua = LWP::UserAgent->new;
-  #my $ua = LWP::UserAgent::JSON->new;
   $ua->agent("MyApp/0.1 ");
 
   #URL
   my $uri = 'http://resistenciarte.org/api/v1/node?parameters[type]=autores';
   
   # Create a request
-  
-  #my $json = '{"username":"foo","password":"bar"}';
-
   my $req = HTTP::Request->new(GET => $uri);
-  #my $req = HTTP::Request::JSON->new(GET => $uri);
-  #$req->header( 'Content-Type' => 'application/json' );
+ 
   $req->content_type('application/json');
-  #$req->content('query=libwww-perl&mode=dist');
-  $req->content($json);
-  # Pass request to the user agent and get a response back
+ 
   my $res = $ua->request($req);
 
   # Check the outcome of the response
