a3126688979b925354bd500bcfb51aa4cb424800
Diego <diegosperoni1@gmail.com>
2015-10-27 15:51:30 -0300

    Update ClientSculpture.pm
    Así como está, "funciona" , deberíamos agregar el resto de los métodos (que hizo facu-san) y después probar consumiendo.

--- ClientSculpture.pm
+++ ClientSculpture.pm
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+package ClientSculpture;
 use strict;
 use warnings;
 use HTTP::Tiny;
@@ -18,36 +18,36 @@ has name=> (is=>'rw' , isa => 'Str');
 
 
 sub request_author {
-	my $server = 'http://resistenciarte.org/api/v1/';
-	my $ping_endpoint = 'node?parameters[type]=autores';
-	my $url = $server.$ping_endpoint;
-	my $headers = { accept => 'application/json' };
-  	#my ($url, $headers, $attempts) = @_;
-  	my $attempts //= 0;
-  	my $http = HTTP::Tiny->new();
-  	my $response = $http->get($url, {headers => $headers});
-  	if($response->{success}) {
-	    my $content = $response->{content};
-	    my $json = decode_json($content);
-	    ##
-	    my $arrayref = decode_json $json;
-		my $name = shift;
-		foreach my $item( @$arrayref ) { 
-		    # fields are in $item->{Year}, $item->{Quarter}, etc.
-	    	my $author= $item->{title};
-		    if ($author =~ /$name/) {
-				return $json;    
-		    }
-		}      
-	    ##
-  	}
-  	$attempts++;
-  	my $reason = $response->{reason};
-	  if($attempts > 3) {
-	    warn 'Failure with request '.$reason;
-	    die "Attempted to submit the URL $url more than 3 times without success";
-	  }
-  	my $response_code = $response->{status};
+        my $self= shift;
+        my $server = 'http://resistenciarte.org/api/v1/';
+        my $ping_endpoint = 'node?parameters[type]=autores';
+        my $url = $server.$ping_endpoint;
+        my $headers = { accept => 'application/json' };
+        #my ($url, $headers, $attempts) = @_;
+        my $attempts //= 0;
+        my $http = HTTP::Tiny->new();
+        my $response = $http->get($url, {headers => $headers});
+        if($response->{success}) {
+            my $content = $response->{content};
+            my $json = decode_json($content);
+
+                my $name = $self->name;
+                foreach my $item( @$json ) {
+                    # fields are in $item->{Year}, $item->{Quarter}, etc.
+                    if ($item->{title}=~ /$name/) {
+                        return $item->{title};
+
+                    }
+                }
+
+        }
+        $attempts++;
+        my $reason = $response->{reason};
+          if($attempts > 3) {
+            warn 'Failure with request '.$reason;
+            die "Attempted to submit the URL $url more than 3 times without success";
+          }
+        my $response_code = $response->{status};
   # we were rate limited
   if($response_code == 429) {
     my $sleep_time = $response->{headers}->{'retry-after'};
