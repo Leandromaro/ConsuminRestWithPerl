9aab9cc51ace80cc905953991f82b1b587e06cf3
Diego <diegosperoni1@gmail.com>
2015-10-27 16:20:26 -0300

    Update consuming.pl
    En la línea 10 da algo raro, hay que revisar, pero consume bien desde el client

--- consuming.pl
+++ consuming.pl
@@ -4,12 +4,15 @@ use warnings;
 use ClientSculpture;
 use Data::Dumper;               # Perl core module
 use Moose;
-
+use JSON qw/decode_json/;
 
 my $client = ClientSculpture->new(name=>"Alberto");
-my $json = $client;
-my $decoded_json = decode_json( $json );
+my %clients = $client->request_author(); 
 
+#my $json = get($client->request_author());
+#print $client;
+#my $decoded_json = decode_json( $json );
 # Print Json Complete
-
-print Dumper $decoded_json;
+foreach my $key (keys %clients){
+    print "$key\n";
+}
