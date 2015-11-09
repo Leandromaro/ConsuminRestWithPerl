87fe8b947e88685a21900b77cd1856502aaf4df6
beefacundo <beefacundo@users.noreply.github.com>
2015-11-09 09:28:18 -0300

    Update Requesting.pm

--- newPms/Requesting.pm
+++ newPms/Requesting.pm
@@ -3,21 +3,17 @@ package Requesting;
 
 use warnings; 					#good practice
 use strict;						#good practice
-use Data::Dumper;				
 use HTTP::Tiny;
 use Time::HiRes qw/sleep/;
 use JSON qw/decode_json/;
-use Moose;
 
 
 sub request{
-	my ($self, $id) = @_;
+	my ($self, $url) = @_;
 	my $devolution;
-	my $server ='http://resistenciarte.org/api/v1/';
-	my $url = $server.$id;
-        
+
     my $headers = { accept => 'application/json' };
-    my $attempts //= 0;
+    my $attempts //= 0;						
     my $http = HTTP::Tiny->new();
     my $response = $http->get($url, {headers => $headers});
     
@@ -26,20 +22,22 @@ sub request{
         $devolution = decode_json($content);
         
     }
-    
+
     $attempts++;
     my $reason = $response->{reason};
       if($attempts > 3) {
         warn 'Failure with request '.$reason;
         die "Attempted to submit the URL $url more than 3 times without success";
       }
+
+
     my $response_code = $response->{status};
 	
 	# we were rate limited
 	if($response_code == 429) {
 		my $sleep_time = $response->{headers}->{'retry-after'};
 	 	sleep($sleep_time);
-		return rest_request($url, $headers, $attempts);
+		return request($url, $headers, $attempts);
 	}
 
 	return $devolution;
