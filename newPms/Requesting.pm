bf0f546d4a76669552f92337f287446505f29634
leandromaro <leandro.maro@glb-d1135.globant.com>
2015-11-09 11:47:32 -0300

    Wheather pm done

--- newPms/Requesting.pm
+++ newPms/Requesting.pm
@@ -6,21 +6,18 @@ use strict;						#good practice
 use HTTP::Tiny;
 use Time::HiRes qw/sleep/;
 use JSON qw/decode_json/;
-
+use Moose;
 
 sub request{
-	my ($self, $url) = @_;
-	my $devolution;
-
-    my $headers = { accept => 'application/json' };
+    my ($self, $url) = @_;
+	my $headers = { accept => 'application/json' };
     my $attempts //= 0;						
     my $http = HTTP::Tiny->new();
     my $response = $http->get($url, {headers => $headers});
     
     if($response->{success}) {
-        my $content = $response->{content};
-        $devolution = decode_json($content);
-        
+        my $content = $response->{content};   
+        return $content;       
     }
 
     $attempts++;
@@ -29,8 +26,6 @@ sub request{
         warn 'Failure with request '.$reason;
         die "Attempted to submit the URL $url more than 3 times without success";
       }
-
-
     my $response_code = $response->{status};
 	
 	# we were rate limited
@@ -39,8 +34,6 @@ sub request{
 	 	sleep($sleep_time);
 		return request($url, $headers, $attempts);
 	}
-
-	return $devolution;
 }
 
 1;
