aa7296a27d2f6783f7aabed4ff5e8f61fdd19a08
Diego <diegosperoni1@gmail.com>
2015-11-09 21:14:28 -0300

    Update ClientSculpture.pm
    -just committed the missing methods.
    -commented code to make it more understable.
    -we have to check the functionality.

--- newPms/ClientSculpture.pm
+++ newPms/ClientSculpture.pm
@@ -15,14 +15,16 @@ has long=> (is=>'rw' , isa => 'Str');
 has authorID=> (is=>'rw', isa=>'Str');
 has authorName=> (is=>'rw', isa=>'Str');
 
+
+
 ##DONE
 sub request_author {
         my $self = shift;
-        my $requester = Requesting->new();
-
+        my $requester = Requesting->new();##it'll use the default parameter
+        ##REQUEST TO THE SERVER
         my $json = $requester->request();
         my $name = $self->name;
-
+        ##TREATMENT
         foreach my $item( @$json ) {
                 if ($item->{title}=~ /(?i)$name(?i)/) {
                         return $item->{title};
@@ -32,79 +34,80 @@ sub request_author {
 ##DONE
 sub request_image {
         my $self= shift;
+        ##PARAMETERS
         my $id = "/file".$self->id;
         my $requester = Requesting->new(parameter=>$id);
+        ##REQUEST TO THE SERVER
+        my $json = requester->request() 
+        ##TREATMENT
         my $imageUrl = $json->{uri_full},"\n";
         return $imageUrl;
 
 }
 
 
-
+##DONE
 sub request_scult_prox{
+        my $self = shift;
+        ##PARAMETERS
+        my $lat = $self->lat;
+        my $long = $self->long;
+        my $requester = Requesting->new(parameter=>'closest_nodes_by_coord?lat='.$lat.'&lon='.$long);
+        ##REQUEST TO THE SERVER
+        my $response = $requester->request()
+        ##TREATMENT
+        my $content = $response->{content};
+        my $json = decode_json($content);
+        my $count = 0;
+        my %temp;
+        
+         foreach my $item (@$json){
+                my $authId = $self->request_auth_scul($item->{nid});
+                my $auth = $self->request_auth_id($authId);
+                my $image = $self->request_image($item->{nid});
+        
+                my @sal = ( $item->{node_title},
+                          $item->{distance},
+                            $item->{field_ubicacion}{und}[0]{value},
+                            $authId,
+                            $auth,
+                            $image
+                );
+        
+                $temp{"$count"} = \@sal;
+            }
+            return \%temp;
+        }
 
-    my ($self,$lat, $long,$response)=@_;
-    my $json;
-
-    my $content = $response->{content};
-        $json = decode_json($content);
-
-
-
-    my $count = 0;
-    my %temp;
-
-    foreach my $item (@$json){
-        my $authId = $self->request_auth_scul($item->{nid});
-        my $auth = $self->request_auth_id($authId);
-        my $image = $self->request_image($item->{nid});
-
-
-        my @sal = ( $item->{node_title},
-                  $item->{distance},
-                    $item->{field_ubicacion}{und}[0]{value},
-                    $authId,
-                    $auth,
-                    $image
-        );
-
-        $temp{"$count"} = \@sal;
-    }
-
-    return \%temp;
-}
-
-
+##DONE
 sub request_auth_scul {
-
-    my $self = shift;
-    my $id_esc = $self->id;
-    my $json;
-    my $response = shift;
-    my $content = $response->{content};
-    $json = decode_json($content);
-    my $ret = $$json{field_autor}{und}[0]{target_id};
-    return $ret;
+        my $self = shift;
+        my $id_esc = $self->id;
+        my $requester = Requesting->new(parameter=>"node/".$id); 
+        ##REQUEST TO THE SERVER
+        my $content = $requester->request();
+        ##TREATMENT
+        my $json = decode_json($content);
+        my $ret = $$json{field_autor}{und}[0]{target_id};
+        return $ret;
 
 }
 
 sub request_auth_id {
-
-    #parametros
-    my ($self,$id,$response) = @_;
-    my $ret = "undef";
-    my $json;
-    my $content = $response->{content};
-    my $json = decode_json($content);
-
-   foreach my $item (@$json){
-        if ($id == $item->{nid}){
-            $ret = $item->{title};
+        my $self = shift;
+        ##PARAMETERS
+        my $id = $self->idthorID;
+        my $requester = Requesting->new(parameter=>'node?parameters[type]=autores');
+        my $ret = "undef";
+        ##REQUEST TO THE SERVER
+        my $content = $requester->request();
+        ##TREATMENT
+        my $json = decode_json($content);
+        foreach my $item (@$json){
+                if ($id == $item->{nid}){
+                        $ret = $item->{title};
+                }
         }
-    }
-
-    return $ret;
-
+        return $ret;
 }
-
 1;
