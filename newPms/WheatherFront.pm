fb900133be38298ec8043118ea10ff8b65b78919
leandromaro <leandro.maro@glb-d1135.globant.com>
2015-11-12 14:38:09 -0300

    wheater changes

--- newPms/WheatherFront.pm
+++ newPms/WheatherFront.pm
@@ -10,9 +10,8 @@ use Try::Tiny;
 
 sub getWheather{
 	my $serviceCall = Requesting->new;
-	#my $content = $serviceCall->request("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22San%20Fernando%2C%20CHO%2C%20Argentina%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"); 
-	my $content = $serviceCall->request("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places("); 
-    try {
+	my $content = $serviceCall->request("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22San%20Fernando%2C%20CHO%2C%20Argentina%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"); 
+	try {
         my $json = decode_json($content); 
         my $temp = $json->{query}{results}{channel}{item}{condition}{code};
         my $text = $json->{query}{results}{channel}{item}{condition}{text};
