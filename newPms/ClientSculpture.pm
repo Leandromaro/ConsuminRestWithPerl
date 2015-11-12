d4571a24519079103166efa0cffff42e0ec11ddd
Diego <diegosperoni1@gmail.com>
2015-11-12 11:28:05 -0300

    Update ClientSculpture.pm
    fixed functionalities

--- newPms/ClientSculpture.pm
+++ newPms/ClientSculpture.pm
@@ -20,17 +20,27 @@ has sculp_id => (is=>'rw' , isa => 'Str');      ##sculpture ID
 ##DONE
 sub request_author {
         my $self = shift;
-        my $requester = Requesting->new();##it'll use the default parameter
+        my $requester = Requesting->new(parameter=>'node?parameters[type]=autores');
         ##REQUEST TO THE SERVER
-        my $json = $requester->request();
-        my $name = $self->name;
-        ##TREATMENT
-        foreach my $item( @$json ) {
-                if ($item->{title}=~ /(?i)$name(?i)/) {
-                        return $item->{title};
+        my $server = $requester->server;
+        my $parameter = $requester->parameter;
+        my $url = $server.$parameter;
+        my $response = $requester->request("$url");
+        try {
+                my $json = decode_json($response);
+                my $name = $self->name;
+                ##TREATMENT
+                foreach my $item( @$json ) {
+                        if ($item->{title}=~ /(?i)$name(?i)/) {
+                                return $item->{title};
+                        }
                 }
+        } catch {
+                my $answer = "the name received doesn't exist or it's misspelled\n";
+                return $answer;
         }
 }
+
 ##DONE
 sub request_image {
         my ($self, $url) = @_;
@@ -60,45 +70,58 @@ sub request_scult_prox{
         my $long = $self->long;
         my $requester = Requesting->new(parameter=>'closest_nodes_by_coord?lat='.$lat.'&lon='.$long);
         ##REQUEST TO THE SERVER
-        my $response = $requester->request();
+        my $url = $requester->server.$requester->parameter;
+        my $response = $requester->request("$url");
         ##TREATMENT
-        my $content = $response->{content};
-        my $json = decode_json($content);
-        my $count = 0;
-        my %temp;
-        
-         foreach my $item (@$json){
-                my $authId = $self->request_auth_scul($item->{nid});
-                my $auth = $self->request_auth_id($authId);
-                my $image = $self->request_image($item->{nidau});
-        
-                my @sal = ( $item->{node_title},
-                          $item->{distance},
-                            $item->{field_ubicacion}{und}[0]{value},
-                            $authId,
-                            $auth,
-                            $image
-                );
-        
-                $temp{"$count"} = \@sal;
+        try {
+                my $json = decode_json($response);
+                my $count = 0;
+                my %temp;
+
+                foreach my $item (@$json){
+                        my $authId = $self->request_auth_scul($item->{nid});
+                        my $auth = $self->request_auth_id($authId);
+                        my $image = $self->request_image($item->{nidau});
+
+                        my @sal = ( $item->{node_title},
+                                $item->{distance},
+                                $item->{field_ubicacion}{und}[0]{value},
+                                $authId,
+                                $auth,
+                                $image
+                        );
+
+                        $temp{"$count"} = \@sal;
+                }
+                return \%temp;
+        } catch {
+                my $answer = "there's not nearby sculptures around you\n";
+                return $answer;
         }
-            return \%temp;
 }
+
 
 ##DONE
 ##given a sculpture id brings info about that sculpture and the author id
 sub request_auth_scul {
         my $self = shift;
-        my $sculp_id = $self->sculp_id; 
-        my $requester = Requesting->new(parameter=>"node/".$sculp_id); 
+        my $sculp_id = $self->sculp_id;
+        my $requester = Requesting->new(parameter=>'node/'."$sculp_id");
         ##REQUEST TO THE SERVER
-        my $content = $requester->request();
+        my $url = $requester->server.$requester->parameter;
+        my $content = $requester->request("$url");
         ##TREATMENT
-        my $json = decode_json($content);
-        my $ret = $$json{field_autor}{und}[0]{target_id};
-        return $ret;
+        try {
+                my $json = decode_json($content);
+                my $ret = $$json{field_autor}{und}[0]{target_id};
+                return $ret;
+        } catch {
+                my $answer = "invalid sculpture ID\n";
+                return $answer;
+        }
 
 }
+
 ##DONE
 ##given an author ID brigns the author name
 sub request_auth_id {
@@ -106,17 +129,25 @@ sub request_auth_id {
         ##PARAMETERS
         my $author_id = $self->author_id;
         my $requester = Requesting->new(parameter=>'node?parameters[type]=autores');
+        my $url = $requester->server.$requester->parameter;
         my $ret = "undef";
         ##REQUEST TO THE SERVER
-        my $content = $requester->request();
+        my $content = $requester->request("$url");
         ##TREATMENT
-        my $json = decode_json($content);
-        foreach my $item (@$json){
-                if ($author_id == $item->{nid}){
-                        $ret = $item->{title};
+        try {
+                my $json = decode_json($content);
+                foreach my $item (@$json){
+                        if ($author_id == $item->{nid}){
+                                $ret = $item->{title};
+                        }
                 }
+                return $ret;
+        } catch {
+                my $answer = "invalid author ID\n";
+                return $answer;
         }
-        return $ret;
+
 }
+
 1;
 
