fc82b5e550fc100b97bb93a28952fc1b069a4878
Diego <diegosperoni1@gmail.com>
2015-11-12 12:22:22 -0300

    Update test.pl
    Added the missing methods

--- newPms/test.pl
+++ newPms/test.pl
@@ -29,9 +29,51 @@ my @menu1_choices = (
                   print $url_text, "\n";
                   $menu1->print();
       }},
-    { text => 'Go to Menu2',
-      code => sub { $menu1->print(); }},
+    { text => 'Check if an author exists',
+      code => sub {
+                print "Please insert the author name\n";
+                my $name = <>;
+                chomp ($name);
+                my $clientSculpture = ClientSculpture->new(name=>"$name");
+                my $fullName = $clientSculpture->request_author();
+                print  "full name: ".$fullName."\n";
+                $menu1->print();
+      }},
+    { text => 'Get nearby sculptures',
+        code => sub {
+                print "Insert a latitude value\n";
+                my $lat = <STDIN>;
+                chomp ($lat);
+                print "Insert a longitude value\n";
+                my $long = <STDIN>;
+                chomp ($long);
+                my $clientSculpture = ClientSculpture->new(lat=>"$lat", long=>"$long");
+                my %hash = $clientSculpture->request_scult_prox();
+                print Dumper(\%hash)."\n";
+                $menu1->print();
+        }},
+   { text => 'Get an author using sculpture ID',
+        code => sub {
+                print "Insert a valid sculpture ID\n";
+                my $id = <>;
+                chomp ($id);
+                my $clientSculpture= ClientSculpture->new(sculp_id=>"$id");
+                my $ret = $clientSculpture->request_auth_scul();
+                print $ret."\n";
+                $menu1->print();
+        }},
+   { text => 'Get an author name using author ID',
+        code =>sub {
+                print "Insert a valid author ID\n";
+                my $id = <>;
+                chomp ($id);
+                my $clientSculpture = ClientSculpture->new(auth_id=>"$id");
+                my $ret = $clientSculpture->request_auth_id();
+                print $ret."\n";
+                $menu1->print();
+        }},
 );
+
 
 # Build menu1
 $menu1 = Menu->new(
